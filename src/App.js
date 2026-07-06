a4d163f6b4302533f99d68f1ab4b5a7b635ab597
paul correa <paulcj2001@hotmail.com>
2026-07-05 21:59:22 -0300

    chore: scaffold app React (CRA) con dependencias y estilos base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function App() {
+  return (
+    <Container className="py-4" style={{ maxWidth: 720 }}>
+      <h1>Alta de empleado</h1>
+      <p>La aplicación está en construcción.</p>
+    </Container>
+  );
+}
